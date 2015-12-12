8bdec1f934a0e424c7939756e56ac821c686fe95
Jonas Schnelli <dev@jonasschnelli.ch>
2015-12-12 15:30:01 +0100

    Fix depends system (permissions, add patches)

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,19 +1,19 @@
-packages:=boost openssl libevent hidapi curl
-darwin_packages:=zeromq
-linux_packages:=zeromq
-native_packages := native_ccache native_comparisontool
+packages:=openssl libevent hidapi curl
+darwin_packages:=
+linux_packages:=
+native_packages :=
 
-qt_native_packages = native_protobuf
-qt_packages = qrencode protobuf
+qt_native_packages = 
+qt_packages = qrencode
 
 qt_linux_packages= qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
 
 
-wallet_packages=bdb
+wallet_packages=
 
-upnp_packages=miniupnpc
+upnp_packages=
 
 ifneq ($(build_os),darwin)
 darwin_native_packages=native_cctools native_cdrkit native_libdmg-hfsplus
